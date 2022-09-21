892c05a0e19ee12fc531bc7e60424060212b75b5
Mams9 <maameahma@yahoo.com>
2022-09-20 17:35:08 -0700

    Pointers Arrays and Strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+/**
+ * reset_to_98 - this takes the pointer to an int parameter
+ * 		and it also updates the value it points to 98
+ *
+ * @n: int parameter
+ *
+ * Return: Nothing
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_h
+#define MAIN.H
+
+#include <stdio.h>
+
+int _putchar(char c);
+void reset_to_98(int *n);
+void swag int(int *a, int b);
+int _strln(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *strcopy(char *dest, char *src);
+int _atoi(char * s)
+
+#endif /* MAIN_H */
