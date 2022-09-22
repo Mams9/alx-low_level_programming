566fd2e47f6a2c9a4f64197bc2152f1611151874
Mams9 <maameahma@yahoo.com>
2022-09-21 21:08:25 -0700

    More pointers

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,21 +1,23 @@
 #include "main.h"
 
 /**
- * _strncat - Copy a string starting from index 0 of `dest`
+ * _strncat - Concatenate two strings`
  * @dest: string
  * @src: string
- * @n: number of chars to copy over
- * Return: `dest` edited string
+ * @n: number of elements to concatenate in
+ * Return: pointer to resulting `dest`
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
+	int i, c;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
+	for (i = 0; dest[i] != '\0'; i++)
+		;
 
-	for (; n > i; i++)
-		dest[i] = '\0';
-
+	for (c = 0; src[c] != '\0' && n > 0; c++, n--, i++)
+	{
+		dest[i] = src[c];
+	}
 	return (dest);
 }
